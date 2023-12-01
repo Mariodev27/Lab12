48b4cf216011c09454c9f5f2860a1a5a991d3955
Alumno <Alumno@LAB1509-06>
2023-12-01 15:30:49 -0500

    Agregar archivos de proyecto.

---
+++ Lab12/Lab12/Models/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab12.Models
+{
+    internal class TaskModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ Lab12/Lab12/ViewModels/OperacionesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab12.ViewModels
+{
+    internal class OperacionesViewModel
+    {
+
+    }
+}
